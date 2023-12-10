701e910a7d12a1f036b69e93d00d016ea860c188
abdel6602 <abdelrahmangamal66002@gmail.com>
2023-12-10 03:40:21 +0200

    testing CI

--- server/tests/sample.test.js
+++ server/tests/sample.test.js
@@ -1,3 +1,9 @@
-test('testing the testing',  () => {
-    expect(1).toBe(1)
-})
+const {add, subtract} = require('../Controllers/sampleController')
+
+test('testing addition' ,  () => {
+    expect(add(2, 3)).toBe(5)
+});
+
+test('testing subtraction' ,  () => {
+    expect(subtract(3, 2)).toBe(1)
+});
