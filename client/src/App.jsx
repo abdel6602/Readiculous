29b6a0093045b5e05f4ced64538c88bbcdd8f3e3
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2023-12-24 17:24:04 +0200

    Fiex bugs and added routes

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import './App.css';
+import SignUp from "./components/pages/SignUp/SignUp";
+import Login from "./components/pages/Login/Login";
+
+function App() {
+  useEffect(() => {
+    console.log("App component mounted");
+  }, []);
+
+  return (
+    <Fragment>
+      <div className="App">
+        <Routes>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Fragment>  
+  );
+}
+
+export default App;
+
+
+// export default () => {
+//   let app = ( 
+//   <Fragment>
+//     <Routes>
+//       <Route path="/sign-up" exact element={<SignUp />} />
+//     </Routes>
+//   </Fragment>
+//   );
+
+//   return <Fragment>{app}</Fragment>;
+// }
